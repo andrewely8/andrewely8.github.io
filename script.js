01d793f46705632c9ff341e9f2c7e82bd95d4d09
andrewely8 <andrewely08@gmail.com>
2026-06-02 23:44:10 -0400

    added pong game

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function myFunction() {
-        alert("test2");
-      }
+let entireScreen = document.getElementById('entireScreen');
+
+window.addEventListener("mousemove", (e) => {
+  let xPos = e.clientX;
+  let yPos = e.clientY;
+  let size = '600px'
+  let color = '#FFFFFF'
+  
+  let transparency = 'transparent 90%';
+
+  entireScreen.style.background = `radial-gradient( 600px at ${xPos}px ${yPos}px, ${color}, ${transparency})`;
+  
+});
